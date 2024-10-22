33c44a4a38c75f0f8a7bb9468b73bca126984941
洛洛 <111134328+Luotianyi-0712@users.noreply.github.com>
2024-10-22 22:50:12 +0800

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,7 +4,7 @@ GITHUB_URL=https://oauth:${Celesphonia_GH_TOKEN}@github.com/Celesphonia/luotiany
 git clone ${GITHUB_URL} temp
 
 rm -rf temp/*
-mv ./LLOneBotDoc/docs/.vitepress/dist/* temp
+mv ./Celesphonia/docs/.vitepress/dist/* temp
 
 cd ./temp
 
@@ -12,8 +12,8 @@ git config --global init.defaultBranch main
 git remote add origin ${GITHUB_URL}
 git branch -M main
 
-git config --global user.name "Mlikiowa"
-git config --global user.email "nanaeonn@outlook.com"
+git config --global user.name "Miku"
+git config --global user.email "bilibilias@hotmail.com"
 git add *
 git commit -m "docs: auto update"
 git push -f
